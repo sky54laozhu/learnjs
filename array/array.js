1e170d5238de6d3877be79a14aab040b7574c729
zhuweifeng641 <sky54laozhu@163.com>
2016-03-01 22:33:48 +0800

    learn js array

---
+++ array/array.js
@@ -0,0 +1,13 @@
+/**var arr = [1,2,3.14,'Hello',null,true];
+alert(arr.length);
+alert(arr);
+arr.length = 8;
+alert(arr);
+var arr = [1,2,3];
+arr[5] = 'x';
+alert(arr);*/
+var arr = [10,20,'30','xyz'];
+alert(arr.indexOf(10));
+alert(arr.indexOf(20));
+alert(arr.indexOf(30));
+alert(arr.indexOf('30'));
