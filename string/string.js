c842a2dfc63424aa99b8f6d805769c8ac805334f
zhuweifeng641 <sky54laozhu@163.com>
2016-03-01 21:42:50 +0800

    string done!

--- string/string.js
+++ string/string.js
@@ -1,4 +1,4 @@
-var age = 15;
+/**var age = 15;
 if(age >= 18){
   alert('adult');
 }else{
@@ -6,3 +6,25 @@ if(age >= 18){
 }
 alert(NaN === NaN);
 alert(isNaN(NaN));
+alert('I\'m \"OK\"');
+alert('\x41');
+alert('\u4e2d\u6587');
+alert(`多行
+字符串
+测试`);*/
+var s = 'Hello, world!';
+/**alert(s);
+alert(s.length);
+for(var i = 0;i<s.length;i++){
+   alert(s[i]);
+}
+alert(s[13]);
+s[0] = 'X';
+alert(s);
+alert(s[0]);
+alert(s.toUpperCase());
+alert(s.toLowerCase()); 
+alert(s.indexOf('world'));
+alert(s.indexOf('World'));*/
+alert(s.substring(0,5));
+alert(s.substring(7));
