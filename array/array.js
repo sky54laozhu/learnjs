2033631f3e06fc8abd7570e14206aa744d66f58c
zhuweifeng641 <sky54laozhu@163.com>
2016-03-07 22:31:26 +0800

    array over

--- array/array.js
+++ array/array.js
@@ -26,10 +26,48 @@ var arr = [1, 2];
 arr.unshift('A','B');
 alert(arr);
 arr.shift();
-alert(arr);*/
+alert(arr);
 var arr = ['B','C','A'];
 alert(arr);
 arr.sort();
 alert(arr);
 arr.reverse();
 alert(arr);
+var arr = ['one','two','three'];
+arr.reverse();
+alert(arr);
+var arr = ['Microsoft','Apple','Yahoo','AOL','Excite','Oracle'];
+var arrtemp = [];
+arrtemp = arr.splice(2,3,'Google','Facebook');
+alert(arr);
+alert(arrtemp);
+alert(arr.splice(2,2));
+alert(arr);
+alert(arr.splice(2,0,'Google','Facebook'));
+alert(arr);
+var arr = ['A','B','C'];
+var added = arr.concat([1,2,3]);
+alert(arr);
+alert(added);
+added = arr.concat(1,2,[3,4]);
+alert(added);
+alert(arr.concat(1,2,[3,4]));
+*/
+var arr = ['A','B','C',1,2,3];
+alert(arr);
+alert(arr.join('-'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
